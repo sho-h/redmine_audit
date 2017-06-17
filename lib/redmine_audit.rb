a03efdbd9082dce2d93aa0082d7b280bd0fa0bd9
Sho Hashimoto <sho.hsmt@gmail.com>
2017-06-17 09:22:18 +0900

    Fix sending mail twice under plugins/redmine_audit directory. Stop rake task defines twice.

--- lib/redmine_audit.rb
+++ lib/redmine_audit.rb
@@ -4,27 +4,28 @@ module RedmineAudit
   # Run the classic redmine plugin initializer after rails boot
   class Plugin < ::Rails::Engine
     config.after_initialize do
+      require File.expand_path('../init', __dir__)
+
+      create_hint = true
       # TODO: support Redmine 3.4.0
       plugins_dir = File.join(Bundler.root, 'plugins')
-
-      # Avoid to load twice same plugin.
       Dir.glob(File.join(plugins_dir, '*/redmine_audit.gemspec')) do |gemspec_path|
         Rails.logger.warn('Skip to load redmine_audit plugin installed as gem.')
         Rails.logger.warn('Use plugins directory\'s redmine_audit plugin')
-        return
+        create_hint = false
       end
 
-      require File.expand_path('../init', __dir__)
-
-      # Create text file to Redmine's plugins directory.
-      # The purpose is telling plugins directory to users.
-      path = File.join(plugins_dir, 'redmine_audit')
-      if !File.exists?(path)
-        File.open(path, 'w') do |f|
-          f.write(<<EOS)
+      if create_hint
+        # Create text file to Redmine's plugins directory.
+        # The purpose is telling plugins directory to users.
+        path = File.join(plugins_dir, 'redmine_audit')
+        if !File.exists?(path)
+          File.open(path, 'w') do |f|
+            f.write(<<EOS)
 This plugin was installed as gem wrote to Gemfile.local instead of putting Redmine's plugin directory.
 See redmine_audit gem installed directory.
 EOS
+          end
         end
       end
     end
--- lib/tasks/redmine_audit.rake
+++ lib/tasks/redmine_audit.rake
@@ -13,17 +13,21 @@ Example:
 END_DESC
 
 namespace :redmine do
-  task audit: :environment do
-    # TODO: More better if requires mailer automatically.
-    require_dependency 'mailer'
-    require_relative '../../app/models/mailer.rb'
+  # Avoid to define same task twice.
+  # TODO: stop load twice this .rake file.
+  if !Rake::Task.task_defined?(:audit)
+    task audit: :environment do
+      # TODO: More better if requires mailer automatically.
+      require_dependency 'mailer'
+      require_relative '../../app/models/mailer.rb'
 
-    redmine_ver = Redmine::VERSION
-    advisories = RedmineAudit::Database.new.advisories(redmine_ver.to_s)
-    if advisories.length > 0
-      users = (ENV['users'] || '').split(',').each(&:strip!)
-      Mailer.with_synched_deliveries do
-        Mailer.unfixed_advisories_found(redmine_ver, advisories, users).deliver
+      redmine_ver = Redmine::VERSION
+      advisories = RedmineAudit::Database.new.advisories(redmine_ver.to_s)
+      if advisories.length > 0
+        users = (ENV['users'] || '').split(',').each(&:strip!)
+        Mailer.with_synched_deliveries do
+          Mailer.unfixed_advisories_found(redmine_ver, advisories, users).deliver
+        end
       end
     end
   end
