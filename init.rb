94eea2c34cf68cea0c238000eed5ffa8dde7ae19
Sho Hashimoto <sho.hsmt@gmail.com>
2018-06-03 21:42:58 +0900

    Specify target redmine version.

--- init.rb
+++ init.rb
@@ -3,6 +3,7 @@ Redmine::Plugin.register :redmine_audit do
   author 'Sho Hashimoto'
   description 'Redmine plugin for checking Redmine\'s own vulnerabilities'
   version RedmineAudit::VERSION
+  requires_redmine version_or_higher: '3.3.0'
   url 'https://github.com/sho-h/redmine_audit/'
   author_url 'https://github.com/sho-h/'
 end
