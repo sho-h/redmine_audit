b6fa09347988aff7ee0e888870113ef9ec821c8f
Sho Hashimoto <sho.hsmt@gmail.com>
2018-07-18 00:18:46 +0900

    Use Redmine's plugin.rb if require success.

--- test/unit/redmine_audit/plugin_database_test.rb
+++ test/unit/redmine_audit/plugin_database_test.rb
@@ -3,26 +3,46 @@ require 'test/unit'
 require 'test/unit/rr'
 require 'redmine_audit/plugin_database'
 
-# TODO: use Redmine's Redmine::Plugin
-module Redmine
-  Plugin = Struct.new(:id, :name, :description, :url, :author, :author_url, :version, :settings, :directory) do
-    @registered_plugins = {}
-    class << self
-      attr_reader :registered_plugins
+begin
+  require 'redmine/plugin'
+rescue LoadError
+  module Redmine
+    class Plugin
+      @registered_plugins = {}
+      class << self
+        attr_reader :registered_plugins
 
-      def register(id, &block)
-        plugin = new(id)
-        plugin.instance_eval(&block)
-        registered_plugins[id] = plugin
+        def def_field(*names)
+          class_eval do
+            names.each do |name|
+              define_method(name) do |*args|
+                args.empty? ? instance_variable_get("@#{name}") : instance_variable_set("@#{name}", *args)
+              end
+            end
+          end
+        end
+
+        def register(id, &block)
+          plugin = new(id)
+          plugin.instance_eval(&block)
+          registered_plugins[id] = plugin
+        end
+
+        def all
+          registered_plugins.values.sort
+        end
+
+        def clear
+          registered_plugins = {}
+        end
       end
 
-      def all
-        registered_plugins.values.sort
+      def initialize(id)
+        @id = id.to_sym
       end
 
-      def clear
-        registered_plugins = {}
-      end
+      def_field :name, :version
+      attr_reader :id
     end
   end
 end
@@ -40,9 +60,9 @@ module RedmineAudit
     end
 
     test 'compare versions with vulnerable plugin' do
-      ver = '0.6.0'
-      name = 'Redmine Git Hosting Plugin'
-      Redmine::Plugin.register(:redmine_git_hosting) { self.version = ver; self.name = name }
+      _ver = '0.6.0'
+      _name = 'Redmine Git Hosting Plugin'
+      Redmine::Plugin.register(:redmine_git_hosting) { version _ver; name _name }
       plugin, advisories = *@database.advisories.first
       assert_equal('Redmine Git Hosting Plugin', plugin.name)
       assert_equal('CVE-2013-4663', advisories[0].id)
@@ -51,9 +71,9 @@ module RedmineAudit
     end
 
     test 'compare versions with not vulnerable plugin' do
-      ver = '1.0.0'
-      name = 'Redmine Git Hosting Plugin'
-      Redmine::Plugin.register(:redmine_git_hosting) { self.version = ver; self.name = name }
+      _ver = '1.0.0'
+      _name = 'Redmine Git Hosting Plugin'
+      Redmine::Plugin.register(:redmine_git_hosting) { version _ver; name _name }
       assert_equal({}, @database.advisories)
     end
   end
