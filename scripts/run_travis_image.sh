7c6a257c198576a23b4e91d3ac0baed83d1ac0e8
Sho Hashimoto <sho.hsmt@gmail.com>
2018-07-22 19:15:16 +0900

    Rename env var.

--- scripts/run_travis_image.sh
+++ scripts/run_travis_image.sh
@@ -2,7 +2,7 @@
 
 git clone --branch=${REDMINE_VER} https://github.com/redmine/redmine.git
 cp /database.yml /redmine/config/
-git clone --branch=${BRANCH} https://github.com/${TRAVIS_REPO_SLUG}.git /redmine/plugins/redmine_audit
+git clone --branch=${TRAVIS_BRANCH} https://github.com/${TRAVIS_REPO_SLUG}.git /redmine/plugins/redmine_audit
 cd /redmine && \
    bundle install --without rmagick && \
    bundle exec rake db:migrate &&
