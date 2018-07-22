454746a4a734d427bfa092958b03268a337047df
Sho Hashimoto <sho.hsmt@gmail.com>
2018-07-22 19:27:34 +0900

    Clone only latest files.

--- scripts/run_travis_image.sh
+++ scripts/run_travis_image.sh
@@ -3,9 +3,9 @@
 echo "target redmine version: ${REDMINE_VER}"
 echo "target plugin repository: ${TRAVIS_REPO_SLUG}"
 echo "target plugin branch: ${TRAVIS_BRANCH}"
-git clone --branch=${REDMINE_VER} https://github.com/redmine/redmine.git
+git clone --depth=1 --branch=${REDMINE_VER} https://github.com/redmine/redmine.git
 cp /database.yml /redmine/config/
-git clone --branch=${TRAVIS_BRANCH} https://github.com/${TRAVIS_REPO_SLUG}.git /redmine/plugins/redmine_audit
+git clone --depth=1 --branch=${TRAVIS_BRANCH} https://github.com/${TRAVIS_REPO_SLUG}.git /redmine/plugins/redmine_audit
 cd /redmine
 bundle install --without rmagick
 bundle exec rake db:migrate
