f5d445ca7086cadde3b2e6f5364da787aa291805
Sho Hashimoto <sho.hsmt@gmail.com>
2018-07-22 19:16:01 +0900

    Add debug info.

--- scripts/run_travis_image.sh
+++ scripts/run_travis_image.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+echo "target redmine version: ${REDMINE_VER}"
+echo "target plugin repository: ${TRAVIS_REPO_SLUG}"
+echo "target plugin branch: ${TRAVIS_BRANCH}"
 git clone --branch=${REDMINE_VER} https://github.com/redmine/redmine.git
 cp /database.yml /redmine/config/
 git clone --branch=${TRAVIS_BRANCH} https://github.com/${TRAVIS_REPO_SLUG}.git /redmine/plugins/redmine_audit
