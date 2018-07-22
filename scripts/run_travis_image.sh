acf05925d58a776f5b481fe3e62be139a88f4969
Sho Hashimoto <sho.hsmt@gmail.com>
2018-07-22 19:16:33 +0900

    Execute command separately.

--- scripts/run_travis_image.sh
+++ scripts/run_travis_image.sh
@@ -6,7 +6,7 @@ echo "target plugin branch: ${TRAVIS_BRANCH}"
 git clone --branch=${REDMINE_VER} https://github.com/redmine/redmine.git
 cp /database.yml /redmine/config/
 git clone --branch=${TRAVIS_BRANCH} https://github.com/${TRAVIS_REPO_SLUG}.git /redmine/plugins/redmine_audit
-cd /redmine && \
-   bundle install --without rmagick && \
-   bundle exec rake db:migrate &&
-   bundle exec rake redmine:plugins:test
+cd /redmine
+bundle install --without rmagick
+bundle exec rake db:migrate
+bundle exec rake redmine:plugins:test
